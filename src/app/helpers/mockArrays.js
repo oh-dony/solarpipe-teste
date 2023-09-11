e958a95650e6f3b1cf606932fe1e62b902d1387e
Adonay Douglas <adonay.douglas.profissional@gmail.com>
2023-09-11 04:32:19 -0300

    fix: ultimo build rs

--- src/app/helpers/mockArrays.js
+++ src/app/helpers/mockArrays.js
@@ -4,42 +4,42 @@ export const noticiasEnergiaSolar = [
     titulo: "Nova tecnologia torna painéis solares mais eficientes",
     descricao:
       "Uma inovação recente no campo da energia solar promete aumentar a eficiência dos painéis solares em até 20%, tornando a energia solar ainda mais acessível e sustentável.",
-    capa: "https://source.unsplash.com/1200x1200/?solar,energy",
+    capa: "https://source.unsplash.com/1000x1000/?solar,energy",
   },
   {
     tipo: "Energia Limpa",
     titulo: "Projeto de parque solar gigante é inaugurado",
     descricao:
       "Um dos maiores parques solares do mundo foi inaugurado hoje, fornecendo energia limpa para milhares de residências e reduzindo significativamente as emissões de carbono na região.",
-    capa: "https://source.unsplash.com/1200x1200/?solar,panel",
+    capa: "https://source.unsplash.com/1000x1000/?solar,panel",
   },
   {
     tipo: "Inovação Energética",
     titulo: "Baterias solares de última geração",
     descricao:
       "Pesquisadores apresentam baterias solares de próxima geração que podem armazenar energia solar por períodos mais longos, tornando a energia solar uma opção mais confiável em todo o mundo.",
-    capa: "https://source.unsplash.com/1200x1200/?solar,battery",
+    capa: "https://source.unsplash.com/1000x1000/?solar,battery",
   },
   {
     tipo: "Sustentabilidade",
     titulo: "Cresce o uso de energia solar em residências",
     descricao:
       "Mais e mais pessoas estão adotando a energia solar em suas residências como uma maneira de economizar dinheiro e reduzir sua pegada de carbono, impulsionando a adoção de painéis solares em todo o país.",
-    capa: "https://source.unsplash.com/1200x1200/?solar,home",
+    capa: "https://source.unsplash.com/1000x1000/?solar,home",
   },
   {
     tipo: "Energia Renovável",
     titulo: "Energia solar lidera a transição energética",
     descricao:
       "A energia solar está desempenhando um papel fundamental na transição global para fontes de energia renovável, contribuindo para reduzir a dependência de combustíveis fósseis e combater as mudanças climáticas.",
-    capa: "https://source.unsplash.com/1200x1200/?renewable,solar",
+    capa: "https://source.unsplash.com/1000x1000/?renewable,solar",
   },
   {
     tipo: "Tecnologia Solar",
     titulo: "Novas células solares revolucionárias são anunciadas",
     descricao:
       "Uma empresa de tecnologia anunciou o desenvolvimento de células solares revolucionárias que podem gerar energia mesmo em condições de baixa luminosidade, abrindo novas possibilidades para a energia solar em locais com climas variados.",
-    capa: "https://source.unsplash.com/1200x1200/?solar,technology",
+    capa: "https://source.unsplash.com/1000x1000/?solar,technology",
   },
 ];
 
